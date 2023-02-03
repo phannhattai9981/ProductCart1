4bda69a9c0671a5ddf8468425587dc355613fc5f
phannhattai9981 <you@example.com>
2023-02-03 20:30:15 +0700

    up

--- src/main/java/mvc/entity/Product.java
+++ src/main/java/mvc/entity/Product.java
@@ -1,2 +1,66 @@
-package mvc.entity;public class Product {
+package mvc.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
+    private int id;
+
+    @Column (name = "name")
+    private String name;
+
+    @Column (name = "price")
+    private double price;
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<OrderDetails> orderDetails;
+
+    public Product() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public List<OrderDetails> getOrderDetails() {
+        return orderDetails;
+    }
+
+    public void setOrderDetails(List<OrderDetails> orderDetails) {
+        this.orderDetails = orderDetails;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", orderDetails=" + orderDetails +
+                '}';
+    }
 }
